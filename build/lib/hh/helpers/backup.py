a5e6157756e1400b0f50b57fef2e485d1ce8cf1f
plwg <56353330+plwg@users.noreply.github.com>
2025-02-01 21:34:51 +0800

    Fix incorrect src setup

--- build/lib/hh/helpers/backup.py
+++
@@ -1,9 +0,0 @@
-import shutil
-
-
-def backup_file(path):
-    backup_path = "".join([str(path), ".bak"])
-
-    shutil.copy(path, backup_path)
-
-    return backup_path
--- build/lib/hh/helpers/return_status.py
+++
@@ -1,3 +0,0 @@
-from enum import Enum
-
-STATUS = Enum("STATUS", ["WAIT", "NOWAIT"])
